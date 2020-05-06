f8f1397411c3bbb30fbdadad5f56c1ecac98071b
youssri ahmed <youssri.ahmed@cg-eg.com>
2020-05-06 10:00:20 +0200

    fix setep 2 by adding droupout eval()

--- test2.py
+++ test2.py
@@ -1,106 +1,220 @@
-import matplotlib.pyplot as plt
-
 import torch
-from torch import nn
-from torch import optim
+import numpy as np
+
+# check if CUDA is available
+train_on_gpu = torch.cuda.is_available()
+
+if not train_on_gpu:
+    print('CUDA is not available.  Training on CPU ...')
+else:
+    print('CUDA is available!  Training on GPU ...')
+from torchvision import datasets
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+
+# number of subprocesses to use for data loading
+num_workers = 0
+# how many samples per batch to load
+batch_size = 20
+# percentage of training set to use as validation
+valid_size = 0.2
+
+# convert data to a normalized torch.FloatTensor
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+# choose the training and test datasets
+train_data = datasets.CIFAR10('data', train=True,
+                              download=True, transform=transform)
+test_data = datasets.CIFAR10('data', train=False,
+                             download=True, transform=transform)
+
+# obtain training indices that will be used for validation
+num_train = len(train_data)
+indices = list(range(num_train))
+np.random.shuffle(indices)
+split = int(np.floor(valid_size * num_train))
+train_idx, valid_idx = indices[split:], indices[:split]
+
+# define samplers for obtaining training and validation batches
+train_sampler = SubsetRandomSampler(train_idx)
+valid_sampler = SubsetRandomSampler(valid_idx)
+
+# prepare data loaders (combine dataset and sampler)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+    sampler=train_sampler, num_workers=num_workers)
+valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, 
+    sampler=valid_sampler, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, 
+    num_workers=num_workers)
+
+# specify the image classes
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+# helper function to un-normalize and display an image
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    plt.imshow(np.transpose(img, (1, 2, 0)))  # convert from Tensor image
+# obtain one batch of training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+images = images.numpy() # convert images to numpy for display
+### View an Image in More Detail
+
+# plot the images in the batch, along with the corresponding labels
+fig = plt.figure(figsize=(25, 4))
+# display 20 images
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+    imshow(images[idx])
+    ax.set_title(classes[labels[idx]])
+rgb_img = np.squeeze(images[3])
+channels = ['red channel', 'green channel', 'blue channel']
+
+fig = plt.figure(figsize = (36, 36)) 
+for idx in np.arange(rgb_img.shape[0]):
+    ax = fig.add_subplot(1, 3, idx + 1)
+    img = rgb_img[idx]
+    ax.imshow(img, cmap='gray')
+    ax.set_title(channels[idx])
+    width, height = img.shape
+    thresh = img.max()/2.5
+    for x in range(width):
+        for y in range(height):
+            val = round(img[x][y],2) if img[x][y] !=0 else 0
+            ax.annotate(str(val), xy=(y,x),
+                    horizontalalignment='center',
+                    verticalalignment='center', size=8,
+                    color='white' if img[x][y]<thresh else 'black')
+#Define the Network
+    
+import torch.nn as nn
 import torch.nn.functional as F
-from torchvision import datasets, transforms,models
 
+# define the CNN architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # convolutional layer
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        # max pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
 
+    def forward(self, x):
+        # add sequence of convolutional and max pooling layers
+        x = self.pool(F.relu(self.conv1(x)))
+        return x
 
-data_dir = 'E:\Contact group_delete\deepLearning\Cat_Dog_data\Cat_Dog_data'
+# create a complete CNN
+model = Net()
+print(model)
 
-# TODO: Define transforms for the training data and testing data
-train_transforms = transforms.Compose([transforms.RandomRotation(30),
-                                       transforms.RandomResizedCrop(224),
-                                       transforms.RandomHorizontalFlip(),
-                                       transforms.ToTensor(),
-                                       transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                        std=[0.229, 0.224, 0.225])]) 
+# move tensors to GPU if CUDA is available
+if train_on_gpu:
+    model.cuda()
+import torch.optim as optim
 
-test_transforms = transforms.Compose([transforms.Resize(255),
-                                      transforms.CenterCrop(224),
-                                      transforms.ToTensor(),
-                                      transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                        std=[0.229, 0.224, 0.225])])
+# specify loss function
+criterion = None
 
-
-# Pass transforms in here, then run the next cell to see how the transforms look
-train_data = datasets.ImageFolder(data_dir + '/train', transform=train_transforms)
-test_data = datasets.ImageFolder(data_dir + '/test', transform=test_transforms)
-
-trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
-testloader = torch.utils.data.DataLoader(test_data, batch_size=64)
-
-
-model = models.densenet121(pretrained=True)
-model
-# Freeze parameters so we don't backprop through them
-for param in model.parameters():
-    param.requires_grad = False
-
-from collections import OrderedDict
-classifier = nn.Sequential(OrderedDict([
-                          ('fc1', nn.Linear(1024, 500)),
-                          ('relu', nn.ReLU()),
-                          ('fc2', nn.Linear(500, 2)),
-                          ('output', nn.LogSoftmax(dim=1))
-                          ]))
+# specify optimizer
+optimizer = None
     
-model.classifier = classifier
+# number of epochs to train the model
+n_epochs = 8 # you may increase this number to train a final model
 
-import time
-#ADD data to cuda for encrease speed culculator 
-#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-for device in ['cpu', 'cuda']:
+valid_loss_min = np.Inf # track change in validation loss
+    ## Train the Network
+for epoch in range(1, n_epochs+1):
 
-    criterion = nn.NLLLoss()
-    # Only train the classifier parameters, feature parameters are frozen
-    optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
+    # keep track of training and validation loss
+    train_loss = 0.0
+    valid_loss = 0.0
     
-    model.to(device)
-
-
-epochs = 1
-steps = 0
-running_loss = 0
-print_every = 5
-for epoch in range(epochs):
-    for inputs, labels in trainloader:
-        steps += 1
-        # Move input and label tensors to the default device
-        inputs, labels = inputs.to(device), labels.to(device)
-        
+    ###################
+    # train the model #
+    ###################
+    model.train()
+    for data, target in train_loader:
+        # move tensors to GPU if CUDA is available
+        if train_on_gpu:
+            data, target = data.cuda(), target.cuda()
+        # clear the gradients of all optimized variables
         optimizer.zero_grad()
-        
-        logps = model.forward(inputs)
-        loss = criterion(logps, labels)
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the batch loss
+        loss = criterion(output, target)
+        # backward pass: compute gradient of the loss with respect to model parameters
         loss.backward()
+        # perform a single optimization step (parameter update)
         optimizer.step()
-
-        running_loss += loss.item()
+        # update training loss
+        train_loss += loss.item()*data.size(0)
         
-        if steps % print_every == 0:
-            test_loss = 0
-            accuracy = 0
-            model.eval()
-            with torch.no_grad():
-                for inputs, labels in testloader:
-                    inputs, labels = inputs.to(device), labels.to(device)
-                    logps = model.forward(inputs)
-                    batch_loss = criterion(logps, labels)
-                    
-                    test_loss += batch_loss.item()
-                    
-                    # Calculate accuracy
-                    ps = torch.exp(logps)
-                    top_p, top_class = ps.topk(1, dim=1)
-                    equals = top_class == labels.view(*top_class.shape)
-                    accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-                    
-            print(f"Epoch {epoch+1}/{epochs}.. "
-                  f"Train loss: {running_loss/print_every:.3f}.. "
-                  f"Test loss: {test_loss/len(testloader):.3f}.. "
-                  f"Test accuracy: {accuracy/len(testloader):.3f}")
-            running_loss = 0
-            model.train()
+model.eval()
+# iterate over test data
+for data, target in test_loader:
+    # move tensors to GPU if CUDA is available
+    if train_on_gpu:
+        data, target = data.cuda(), target.cuda()
+    # forward pass: compute predicted outputs by passing inputs to the model
+    output = model(data)
+    # calculate the batch loss
+    loss = criterion(output, target)
+    # update test loss 
+    test_loss += loss.item()*data.size(0)
+    # convert output probabilities to predicted class
+    _, pred = torch.max(output, 1)    
+    # compare predictions to true label
+    correct_tensor = pred.eq(target.data.view_as(pred))
+    correct = np.squeeze(correct_tensor.numpy()) if not train_on_gpu else np.squeeze(correct_tensor.cpu().numpy())
+    # calculate test accuracy for each object class
+    for i in range(batch_size):
+        label = target.data[i]
+        class_correct[label] += correct[i].item()
+        class_total[label] += 1
+
+# average test loss
+test_loss = test_loss/len(test_loader.dataset)
+print('Test Loss: {:.6f}\n'.format(test_loss))
+
+for i in range(10):
+    if class_total[i] > 0:
+        print('Test Accuracy of %5s: %2d%% (%2d/%2d)' % (
+            classes[i], 100 * class_correct[i] / class_total[i],
+            np.sum(class_correct[i]), np.sum(class_total[i])))
+    else:
+        print('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+
+print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
+    100. * np.sum(class_correct) / np.sum(class_total),
+    np.sum(class_correct), np.sum(class_total)))
+#Visualize Sample Test Results
+# obtain one batch of test images
+dataiter = iter(test_loader)
+images, labels = dataiter.next()
+images.numpy()
+
+# move model inputs to cuda, if GPU available
+if train_on_gpu:
+    images = images.cuda()
+
+# get sample outputs
+output = model(images)
+# convert output probabilities to predicted class
+_, preds_tensor = torch.max(output, 1)
+preds = np.squeeze(preds_tensor.numpy()) if not train_on_gpu else np.squeeze(preds_tensor.cpu().numpy())
+
+# plot the images in the batch, along with predicted and true labels
+fig = plt.figure(figsize=(25, 4))
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+    imshow(images[idx])
+    ax.set_title("{} ({})".format(classes[preds[idx]], classes[labels[idx]]),
+                 color=("green" if preds[idx]==labels[idx].item() else "red"))
